68565a55354afda0ab3ef3a2e32613a047f47793
Claus_Zhu <305275506@qq.com>
2015-05-23 01:21:30 +0800

    background
    设置背景

--- PersonalProject/mid335x/src/com/mid335x/MainActivity.java
+++ PersonalProject/mid335x/src/com/mid335x/MainActivity.java
@@ -2,6 +2,8 @@ package com.mid335x;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -9,14 +11,11 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		if(this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+			   setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+			}
 		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		
 	}
 
 }
