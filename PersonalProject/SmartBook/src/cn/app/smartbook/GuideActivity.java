7fb0af6d5dc72cab539125e3e21cb177acd487a3
Claus_Zhu <305275506@qq.com>
2015-07-30 00:22:40 +0800

    引导界面
    创建引导界面，当第一次启动程序时，显示该界面

--- PersonalProject/SmartBook/src/cn/app/smartbook/GuideActivity.java
+++ PersonalProject/SmartBook/src/cn/app/smartbook/GuideActivity.java
@@ -1,11 +1,15 @@
 /**
  * 登陆引导界面
+ * 1、点击进入主界面
+ * 2、判断是否第一次登录
  */
 package cn.app.smartbook;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class GuideActivity extends Activity {
 
@@ -13,13 +17,16 @@ public class GuideActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_guide);
+		
+	}
+	
+	public void enter(View view) {
+		System.out.println("点击进入");
+		Intent intent = new Intent();
+		intent.setClass(this, MainActivity.class);
+		startActivity(intent);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.guide, menu);
-		return true;
-	}
+
 
 }
