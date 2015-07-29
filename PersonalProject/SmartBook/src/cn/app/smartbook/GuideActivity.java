b5548530f7f6e3a0370c71e3d6e55233e1897078
Claus_Zhu <305275506@qq.com>
2015-07-30 01:18:09 +0800

    增加判断启动次数
    看软件是否第一次启动，如果第一次启动，启动引导界面，否则直接显示登录界面

--- PersonalProject/SmartBook/src/cn/app/smartbook/GuideActivity.java
+++ PersonalProject/SmartBook/src/cn/app/smartbook/GuideActivity.java
@@ -8,34 +8,47 @@ package cn.app.smartbook;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.view.Menu;
 import android.view.View;
 
 public class GuideActivity extends Activity {
 	//是否第一次登录
-	private boolean isFirstLoign;
+	private boolean isFirstLogin;
+	//用sharepreference来记录软件信息
+	private SharedPreferences  loginPreferences;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		if(isFirstLoign==true) {
-			setContentView(R.layout.activity_login);
+		//取得SharePreference对象,name:文件名,xml结尾,Mode,存取模式
+		loginPreferences = getSharedPreferences("login_info", MODE_PRIVATE);
+		isFirstLogin = loginPreferences.getBoolean("isFirstLogin", true);
+		//判断软件是否第一次登录
+		if(isFirstLogin==true) {
+			setContentView(R.layout.activity_guide);
+			Editor preferenceEditor = loginPreferences.edit();
+			preferenceEditor.putBoolean("isFirstLogin", false);
+			preferenceEditor.commit();
 		} else {
-			loign();
+			login();
 		}
 		
 	}
 	
 	//启动登录界面
-	private void loign() {
+	private void login() {
 		Intent intent = new Intent();
 		intent.setClass(this, LoginActivity.class);
 		startActivity(intent);
 		
 	}
 
+	//点击进入登录界面
 	public void enter(View view) {
 		System.out.println("点击进入");
+		login();
 		
 	}
 
