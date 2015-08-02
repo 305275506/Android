842b9c6fe9c659b71ebd9560723b5472e183b11b
Claus_Zhu <305275506@qq.com>
2015-08-02 23:02:45 +0800

    登录界面布局

--- PersonalProject/SmartBook/gen/cn/app/smartbook/R.java
+++ PersonalProject/SmartBook/gen/cn/app/smartbook/R.java
@@ -24,10 +24,19 @@ public final class R {
         public static final int click_enter=0x7f020000;
         public static final int guide_background=0x7f020001;
         public static final int ic_launcher=0x7f020002;
+        public static final int login_background=0x7f020003;
+        public static final int login_image=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int account_id=0x7f080004;
+        public static final int action_settings=0x7f080009;
         public static final int imageButton1=0x7f080001;
+        public static final int imageView1=0x7f080002;
+        public static final int linearLayout1=0x7f080003;
+        public static final int linearLayout2=0x7f080005;
+        public static final int loginButton_id=0x7f080007;
+        public static final int password_id=0x7f080006;
+        public static final int registerButton_id=0x7f080008;
         public static final int textView1=0x7f080000;
     }
     public static final class layout {
