5e5a0a1517f547a33e107841b077beb5d8a3384c
Claus_Zhu <305275506@qq.com>
2015-08-16 12:01:03 +0800

    增加测试项
    测试增、删、改、查功能

--- PersonalProject/SmartBook/test/cn/app/smartbook/sqlite/SQLiteTest.java
+++ PersonalProject/SmartBook/test/cn/app/smartbook/sqlite/SQLiteTest.java
@@ -1,5 +1,6 @@
 package cn.app.smartbook.sqlite;
 
+import android.content.ContentValues;
 import android.test.AndroidTestCase;
 /**
  * 测试类，测试数据库
@@ -8,16 +9,63 @@ import android.test.AndroidTestCase;
  */
 public class SQLiteTest extends AndroidTestCase {
 	SQLiteTools tool = null;
-	boolean actual;
+
+	//测试创建数据库
 	public void testCreateSQL() {
+		boolean actual = false;
 		tool = new SQLiteTools();
-		tool.openSQL("user");
+		tool.openSQL(getContext(),"user.db");
 		if(tool!=null) {
 			actual = true;
-		} else {
-			actual = false;
 		}
 		assertEquals(true, actual);
+	}
+	
+	//测试创建表
+	public void testAddTable() {
+		String actual = null;
+		tool = new SQLiteTools();
+		actual = tool.addTable(tool.openSQL(getContext(),"user.db"),"records");
+		assertEquals("records", actual);
+	}
+	
+	//测试往表中加入参数
+	public void testAddPara() {
+		String actual = null;
+		tool = new SQLiteTools();
+//		actual = tool.addPara(tool.openSQL(getContext(),"user.db"), "records", "account", "integer");
+		tool.addPara(tool.openSQL(getContext(),"user.db"), "records", "name", "text");
+		tool.addPara(tool.openSQL(getContext(),"user.db"), "records", "password", "integer");
+//		assertEquals("account", actual);
+	}
+	
+	//测试插入记录功能
+	public void testInsertValue() {
+		tool = new SQLiteTools();
+		ContentValues values = new ContentValues();
+		values.put("name", "小红");
+		values.put("password", 345);
+		values.put("account", 305275509);
+		tool.insertValue(tool.openSQL(getContext(), "user.db"), "records", values);
+		values.put("name", "小陈");
+		values.put("password", 3459993);
+		values.put("account", 305275510);
+		tool.insertValue(tool.openSQL(getContext(), "user.db"), "records", values);
+	}
+	
+	//测试删除记录功能
+	public void testDeleteValue() {
+		tool = new SQLiteTools();
+		tool.deleteValue(tool.openSQL(getContext(), "user.db"), "records", "account","305275508");
+		tool.deleteValue(tool.openSQL(getContext(), "user.db"), "records", "name","小陈");
+	}
+	
+	//测试更新记录功能
+	public void testUpdate() {
+		tool = new SQLiteTools();
+		ContentValues values = new ContentValues();
+		values.put("name", "老刘");
+		tool.update(tool.openSQL(getContext(), "user.db"), "records", values, "account", "305275507");
 	}
 
 }
